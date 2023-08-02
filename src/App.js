38bec418baff1a5fc3122f91b4ece2c1bdbf190a
Rishikant <goswami.rishikant@gmail.com>
2023-08-02 23:13:31 +0530

    REACT-005 video 13,14,15,16,17,18,19 errors and refactoring

--- src/App.js
+++ src/App.js
@@ -3,27 +3,50 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert'
+import About from './components/About'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const commonProps = { title: 'First Menu', aboutText: 'About Us' };
 function App() {
   const [mode, setMode] = useState('light'); //whether dark mode is enabled
+  const [alert, setAlert] = useState(null)
+
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showAlert('Dark mode enabled successfully', 'success')
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert('Light mode enabled successfully', 'success')
     }
   }
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }
+
   return (
     <>
-      <Navbar  {...commonProps} mode={mode} toggleMode={toggleMode} />
-      <div className="container my-3">
-        <TextForm heading="Enter the text" mode={mode} />
-        {/* <About /> */}
-      </div>
+      <Router>
+        <Navbar  {...commonProps} mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" exact element={<About mode={mode} />} />
+            <Route path="/" exact element={<TextForm heading="Enter the text" showAlert={showAlert} mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,25 +6,39 @@ export default function TextForm(props) {
     const handleUpClick = () => {
         // console.log('click event triggered');
         setText(text.toUpperCase());
+        props.showAlert('converted to uppercase', 'success')
     }
     const handleOnchange = (event) => {
         setText(event.target.value);
     }
     const handleLwClick = (event) => {
         setText(text.toLowerCase());
+        props.showAlert('converted to lowercase', 'success')
+
     }
 
     const handleClearClick = (event) => {
         let clerTxt = '';
         setText(clerTxt);
+        props.showAlert('Text cleared', 'success')
+
     }
 
     const handleExtraSpaces = (event) => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "))
+        props.showAlert('Removed extra spaces', 'success')
+
     }
 
-    const [text, setText] = useState('')
+    const [text, setText] = useState('');
+
+    // const unCountExtraSpace = (word) => {
+    //     let newWord = word.replace(/\s+/g, ' ').trim();
+    //     let totWord = newWord.split(" ")[newWord.split(" ").length - 1] === '' ? newWord.split(" ").slice(0, newWord.split(" ").length - 1).length : newWord.split(" ").length;
+    //     return totWord;
+    //     // text.trim() === '' ? 0 : text.match(/\S+/g).length
+    // }
 
     return (
         <>
@@ -32,17 +46,17 @@ export default function TextForm(props) {
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
                     {/* <label htmlFor="myBox" className="form-label">Example textarea</label> */}
-                    <textarea className="form-control" value={text} onChange={handleOnchange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnchange} style={{ backgroundColor: props.mode === 'dark' ? '#134 66e' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }} id="myBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
-                <button className="btn btn-primary mx-2" onClick={handleLwClick}>Convert to lowercase</button>
-                <button className="btn btn-warning mx-2" onClick={handleClearClick}>Clear text</button>
-                <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove extra spaces</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to uppercase</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1" onClick={handleLwClick}>Convert to lowercase</button>
+                <button disabled={text.length === 0} className="btn btn-warning mx-2 my-1" onClick={handleClearClick}>Clear text</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-2 my-1" onClick={handleExtraSpaces}>Remove extra spaces</button>
             </div >
             <div className="container my-2" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
                 <h1>Srisha my darling daughter</h1>
-                <p>{text.split(" ").length} words, {text.length} characters</p>
-                <p>{0.008 * text.split(" ").length}Minutes read</p>
+                <p>{text.split(" ").filter((tx) => { return tx.length != 0 }).length} words, {text.length} characters</p>
+                <p>{0.008 * text.split(" ").filter((tx) => { return tx.length != 0 }).length}Minutes read</p>
                 <p>{text.length > 0 ? text : 'Please enter your text'}</p>
             </div>
         </>
